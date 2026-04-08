84f5cd514b55a512281e69d2d1912731a3be19f4
Carla-FL <carla.flpro@gmail.com>
2026-04-08 17:09:59 +0200

    ajout pipeline CI/CD + feature flags version1

---
+++ config/flags.py
@@ -0,0 +1,7 @@
+# Interrupteurs de fonctionnalités
+# True = visible en prod / False = masqué
+FEATURES = {
+    "analyse_sentiment": True,
+    "comparaison_concurrents": False,
+    "export_pdf": False,
+}
