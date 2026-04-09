9c79e5c59fe9fecd1e49a46f17eacbe18e7b47f9
Carla-FL <carla.flpro@gmail.com>
2026-04-09 11:54:25 +0200

    dévelopement des tests unitaires sur la fonction url2id avec différents cas

--- tests/unitaires/test_utils.py
+++ tests/unitaires/test_utils.py
@@ -2,25 +2,101 @@ import pytest
 from src.utils import url2id
 from streamlit.testing.v1 import AppTest
 
-# AAA (Arrange, Act, Assert)
+# ========================================
+# TESTS UNITAIRES pour les fonctions de utils.py
+# fonctions pure, pas de dépendances
+# Pattern AAA (Arrange, Act, Assert)
+# ___AAA___
+# Arrange : préparer les données d'entrée et les résultats attendus
+# Act : exécuter la fonction à tester
+# Assert : vérifier que le résultat correspond à ce qui est attendu
+# ========================================
 
-def test_url2id():
 
-    # arrange prépare les données d'entrée et les résultats attendus
+# ======================================== URL2ID ================================================================================
+# _______URL COURT VALIDE__________
+def test_url2id_url_court():
+    """ test une url au format court et vérifie que la fonction url2id extrait correctement l'id de la vidéo."""
+    # Arrange 
     url = "https://youtu.be/68QYq9jcEIQ"
-
-    # act exécute la fonction à tester
+    # Act
     result = url2id(url)
-
-    # assert vérifie que le résultat correspond à ce qui est attendu
+    # Assert
     assert result == "68QYq9jcEIQ", f"Expected '68QYq9jcEIQ' but got {result}"
 
-def test_url2id_erreur():
+def test_url2id_url_court_taille_id():
+    """ test une url au format court et vérifie que la fonction url2id extrait bien un id de taille 11"""
+    url = "https://youtu.be/68QYq9jcEIQ"
+    result = url2id(url)
+    assert len(result) == 11, f"Expected id length of 11 but got {len(result)}"
+
+# _______URL LONG VALIDE__________
+def test_url2id_url_longue():
+    """ test une url au format long et vérifie que la fonction url2id extrait correctement l'id de la vidéo."""
+    url = "https://www.youtube.com/watch?v=68QYq9jcEIQ"
+    result = url2id(url)
+    assert result == "68QYq9jcEIQ", f"Expected '68QYq9jcEIQ' but got {result}"
+
+def test_url2id_url_longue_taille_id():
+    """ test une url au format long et vérifie que la fonction url2id extrait bien un id de taille 11"""
+    url = "https://www.youtube.com/watch?v=68QYq9jcEIQ"
+    result = url2id(url)
+    assert len(result) == 11, f"Expected id length of 11 but got {len(result)}"
+
+# _______URL INVALIDE__________
+def test_url2id_url_vide():
+    """ test une url vide et vérifie que la fonction url2id lève une exception ValueError, indiquant que l'URL est invalide."""
     with pytest.raises(ValueError): # on s'assure que la fonction me donne bien l'erreur attendu et programmé
-        url2id("https://youtube/68QYq9jcEIQ")
-        url2id("http://youtu.be/68QYq9jcEIQ")
-        url2id("https://youtube/68QYq9jcEIbvjhvjhvjvcQ")
         url2id("")
+
+
+# _______URL COURT INVALIDE__________
+def test_url2id_url_court_http():
+    """ test une url au format court mais avec http au lieu de https et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("http://youtu.be/68QYq9jcEIQ")
+
+def test_url2id_url_court_id_invalide():
+    """ test une url au format court mais avec un id de vidéo invalide (trop long) et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://youtu.be/68QYq9jcEIbvjhvjhvjvcQ")
+
+def test_url2id_url_court_non_youtube():
+    """ test une url au format court qui ne correspond pas à un format YouTube valide et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://youtube.com/68QYq9jcEIQ")
+
+def test_url2id_url_court_sans_id():
+    """ test une url au format court qui ne contient pas d'id de vidéo et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://www.youtu.be/")
+
+
+# _______URL LONG INVALIDE__________
+def test_url2id_url_long_http():
+    """ test une url au format long mais avec http au lieu de https et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("http://www.youtube.com/watch?v=68QYq9jcEIQ")
+
+def test_url2id_url_longue_id_invalide():
+    """ test une url au format long mais avec un id de vidéo invalide (trop long) et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://www.youtube.com/watch?v=68QYq9jcEIbvjhvjhvjvcQ")
+
+
+def test_url2id_url_long_non_youtube():
+    """ test une url au format long qui ne correspond pas à un format YouTube valide et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://www.youtu.be/watch?v=68QYq9jcEIQ")
+
+
+def test_url2id_url_long_sans_id():
+    """ test une url au format long qui ne contient pas d'id de vidéo et vérifie que la fonction lève une exception ValueError."""
+    with pytest.raises(ValueError):
+        url2id("https://www.youtube.com/watch?v=")
+
+# ======================================== INITIALIZE SESSION STATE ================================================================================
+
 
 def test_get_url():
     pass
